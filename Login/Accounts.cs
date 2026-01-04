17eb3721c74fab168681463a698b957cd8b64caa
Gamer <Gamer@ASUS>
2026-01-04 18:05:10 +0000

    Add project files.

---
+++ Login/Accounts.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class studentAccounts
+{
+
+
+    private string studentUserName;
+    private string studentPassword;
+    
+    public string StudentName
+    {
+        get { return studentUserName; }
+        set { studentUserName = value; }
+    }
+    public string StudentPassword
+    {
+        get { return studentPassword; }
+        set { studentPassword = value; }
+    }
+
+
+}
+
+public class adminAccounts
+{
+    private string adminUsername;
+    private string adminPassword;
+
+    public string adminName
+    {
+        get { return adminUsername; }
+        set { adminUsername = value; }
+    }
+    public string adminPass
+    {
+        get { return adminPassword; }
+        set { adminPassword = value; }
+    }
+}
+
+public static class UserAccounts
+{
+    public static List<studentAccounts> studentList = new List<studentAccounts>() //list of student accounts
+    {
+        new studentAccounts
+        {
+            StudentName = "John",
+            StudentPassword = "1234"
+        }
+    };
+
+    public static List<adminAccounts> adminList = new List<adminAccounts>()  //List of the admin accounts
+    {
+        new adminAccounts
+        {
+            adminName = "Ronan",
+            adminPass = "Password"
+        }
+    };
+}
---
+++ Login/Login.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class LoginMainMenu
+{
+
+
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("Please Select an Option" +
+                "\n 1). Student Login" +
+                "\n 2). Admin Login" +
+                "\n 3). Exit Program");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    StudentLogin(); //This takes the user to the student login screen
+                    break;
+
+                case "2":
+                    AdminLogin(); //This takes the user to the admin login screen
+                    break;
+
+                case "3":
+                    Console.WriteLine("End Program");
+                    return; //This ends the program
+
+                default:
+                    Console.WriteLine("Invalid option - Please select one of the 3 options above");
+                    continue; //This repeats the menu if the user doesnt select one of the options
+            }
+        }
+    }
+
+    static void StudentLogin()
+    {
+        Console.WriteLine("Welcome To the Student Login\n");
+
+        Console.Write("Username: ");
+        string inputStudentUsername = Console.ReadLine();
+
+        Console.Write("Password: ");
+        string inputStudentPassword = Console.ReadLine();
+
+        bool checkStudent = UserAccounts.studentList.Any(s =>
+        s.StudentName == inputStudentUsername &&
+        s.StudentPassword == inputStudentPassword);
+
+        if (checkStudent)
+        {
+            Console.WriteLine("Student successful Welcome " + inputStudentUsername);
+        }
+        else
+        {
+            Console.WriteLine("Student login unsuccessful,");
+        }
+    }
+
+    static void AdminLogin()
+    {
+        Console.WriteLine("Welcome To the Admin Login\n");
+
+        Console.Write("Username: ");
+        string inputAdminUsername = Console.ReadLine();
+
+        Console.Write("Password: ");
+        string inputAdminPassword = Console.ReadLine();
+
+        bool checkAdmin = UserAccounts.adminList.Any(a =>
+        a.adminName == inputAdminUsername &&
+        a.adminPass == inputAdminPassword);
+
+        if (checkAdmin)
+        {
+            Console.WriteLine("Admin Login Successful, welcome " + inputAdminUsername);
+        }
+        else
+        {
+            Console.WriteLine("Admin login unsuccessful");
+        }
+    }
+}
